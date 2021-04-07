5278eccdae48253a3178667eea3c9f08c05518f4
Vitrai Gábor <vitraig@gmail.com>
2021-04-07 23:40:51 +0200

    Code Formatting for Documentation

--- SQL Assignment/database DDL/ullman_tunnel.sql
+++ SQL Assignment/database DDL/ullman_tunnel.sql
@@ -12,7 +12,11 @@ alter table races drop column "CIRCUITID";
 -- Fastest Lap times for every Constructor for every race in 2010
 CREATE OR REPLACE VIEW FASTEST_LAPS_OF_CONSTRUCTORS_WITH_PILOTS AS
     SELECT races.year, races.name as RaceName, constructors.name, results.fastestLapSpeed
-    FROM constructors INNER JOIN (drivers INNER JOIN (races INNER JOIN results ON races.raceId = results.raceId) ON drivers.driverId = results.driverId) ON constructors.constructorId = results.constructorId
+    FROM constructors INNER JOIN 
+        (drivers INNER JOIN 
+            (races INNER JOIN results ON races.raceId = results.raceId) 
+        ON drivers.driverId = results.driverId) 
+    ON constructors.constructorId = results.constructorId
     WHERE ((races.year) = 2010);
     
 
@@ -20,7 +24,12 @@ CREATE OR REPLACE VIEW FASTEST_LAPS_OF_CONSTRUCTORS_WITH_PILOTS AS
 -- Drivers with podium positions of on the Hungaroring
 CREATE OR REPLACE VIEW POL_POSITION_DRIVERS_ON_HUNGARORING AS
     SELECT drivers.forename, drivers.surname, drivers.nationality, ID, Position
-    FROM drivers INNER JOIN (races INNER JOIN (SELECT driverId as ID, POSITIONTEXT as Position FROM results WHERE POSITIONTEXT = '1' OR POSITIONTEXT = '2' OR POSITIONTEXT = '3') ON races.raceId = ID) ON drivers.driverId = ID
+    FROM drivers INNER JOIN 
+        (races INNER JOIN 
+            (SELECT driverId as ID, POSITIONTEXT as Position FROM 
+            results WHERE POSITIONTEXT = '1' OR POSITIONTEXT = '2' OR POSITIONTEXT = '3') 
+        ON races.raceId = ID) 
+    ON drivers.driverId = ID
     GROUP BY drivers.forename, drivers.surname, races.name, drivers.nationality, ID, Position
     HAVING (((races.name) = 'Hungarian Grand Prix'));
     
@@ -28,9 +37,13 @@ CREATE OR REPLACE VIEW POL_POSITION_DRIVERS_ON_HUNGARORING AS
 -- View 3: Query using a set operator!
 -- Who is an italian driver or drives an italian car
 CREATE OR REPLACE VIEW ITALIAN_DRIVERS AS
-    (SELECT DISTINCT results.driverid FROM drivers INNER JOIN results ON results.driverId = drivers.driverId WHERE drivers.nationality = 'Italian')
+    (SELECT DISTINCT results.driverid FROM drivers 
+        INNER JOIN results ON results.driverId = drivers.driverId 
+        WHERE drivers.nationality = 'Italian')
     UNION
-    (SELECT DISTINCT results.driverid FROM constructors INNER JOIN results ON results.constructorId = constructors.constructorId WHERE constructors.nationality = 'Italian');
+        (SELECT DISTINCT results.driverid FROM constructors 
+        INNER JOIN results ON results.constructorId = constructors.constructorId 
+        WHERE constructors.nationality = 'Italian');
 
 
 -- PLSQL function : Which queries one of your view, using parameter(s) and cursor!
@@ -38,7 +51,8 @@ CREATE OR REPLACE VIEW ITALIAN_DRIVERS AS
 CREATE OR REPLACE FUNCTION hungaroring_filter(nationality_param VARCHAR2)
 RETURN NUMBER
 is
-    CURSOR curs1 IS SELECT forename, surname, nationality, id, position FROM POL_POSITION_DRIVERS_ON_HUNGARORING WHERE nationality = nationality_param;
+    CURSOR curs1 IS SELECT forename, surname, nationality, id, position 
+    FROM POL_POSITION_DRIVERS_ON_HUNGARORING WHERE nationality = nationality_param;
     rec curs1%ROWTYPE;
     counter int;
 BEGIN
@@ -65,7 +79,8 @@ create or replace procedure generate_code is
     rec curs1%ROWTYPE;
 BEGIN
     for rec in curs1 loop
-        update drivers set drivers.code = UPPER(SUBSTR(drivers.surname,1,3)) where current of curs1;
+        update drivers set drivers.code = UPPER(SUBSTR(drivers.surname,1,3)) 
+        where current of curs1;
     end loop;
 END;
 
